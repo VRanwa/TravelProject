dd3b9127a56809b36bb2d4371d3ffb6580f91df2
VRanwa <123167435+VRanwa@users.noreply.github.com>
2024-07-17 10:54:07 +0530

    Add files via upload

---
+++ JScript.js
@@ -0,0 +1,26 @@
+function validateInput() {
+    const username = document.querySelector('.inpu1[type="text"]').value;
+    const password = document.querySelector('.inpu1[type="password"]').value;
+  
+
+    if (username.length < 6 || password.length < 6) {
+        alert('Username and password must be at least 6 characters long.');
+        return false;
+      }
+    
+      if (!/^\d+$/.test(password)) {
+        alert('Password should contain only numbers.');
+        return false;
+      }
+
+    if (username === "vikas@gmail.com" && password === "123456") {
+      window.location.href = "index.html";
+      return true;
+    } else {
+      alert('Invalid username or password.');
+      return false;
+    }
+  
+    return true;
+  }
+  
